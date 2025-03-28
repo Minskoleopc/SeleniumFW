4693f11fefbc3e6c3a647f25b194c12e97a08caf
gitminskole <git.minskole@gmail.com>
2025-03-28 18:49:15 +0530

    added

--- src/test/java/tests/GoogleSearch.java
+++ src/test/java/tests/GoogleSearch.java
@@ -1,23 +1,32 @@
 package tests;
 
-import base.BaseTest;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.testng.Assert;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
+
+import base.BaseTest;
 import utilities.RetryAnalyzer;
 import utilities.TestListener;
 
 @Listeners(TestListener.class)
 public class GoogleSearch extends BaseTest {
-
+	
+	private static final Logger logger = LogManager.getLogger(GoogleSearch.class);
+	
     @Test(retryAnalyzer = RetryAnalyzer.class)
     public void googleSearch() {
+    	
+    	logger.info("Starting the testcase  and browser launched successfully");
         driver.get("https://www.google.com");
         driver.findElement(By.name("q")).sendKeys("Selenium TestNG", Keys.ENTER);
-
+       
         boolean resultDisplayed = driver.findElement(By.id("search")).isDisplayed();
         Assert.assertTrue(resultDisplayed, "Search results not displayed");
+        logger.info("Test case  successfully");
     }
 }
